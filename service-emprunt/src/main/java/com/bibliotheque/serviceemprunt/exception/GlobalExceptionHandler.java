8764b7c9fc70ec5edfe8fb9ce54a690a63c63368
Ngue-Hamilton <nguefreshnel@gmail.com>
2026-04-28 13:13:41 +0100

    refactor: déplacement dans package exception

--- service-emprunt/src/main/java/com/bibliotheque/serviceemprunt/exception/GlobalExceptionHandler.java
+++ service-emprunt/src/main/java/com/bibliotheque/serviceemprunt/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.bibliotheque.serviceemprunt;
+package com.bibliotheque.serviceemprunt.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- service-emprunt/src/main/java/com/bibliotheque/serviceemprunt/exception/ResourceNotFoundException.java
+++ service-emprunt/src/main/java/com/bibliotheque/serviceemprunt/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.bibliotheque.serviceemprunt;
+package com.bibliotheque.serviceemprunt.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
